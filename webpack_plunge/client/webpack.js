c7ce4cfe7b6da61977b9551a1835a1f4799fb6da
Gene Conroy-Jones <drgenejones@gmail.com>
2017-02-14 18:41:37 -0800

    Add deserver and include Angular

--- webpack_plunge/client/webpack.js
+++ webpack_plunge/client/webpack.js
@@ -1,2 +1,3 @@
+require('../node_modules/angular/angular.js')
 require("file-loader?name=index.html!./index.html");
 require('./app/app.js');
--- webpack_plunge/webpack.config.js
+++ webpack_plunge/webpack.config.js
@@ -7,6 +7,10 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 9000
     }
-
 };
