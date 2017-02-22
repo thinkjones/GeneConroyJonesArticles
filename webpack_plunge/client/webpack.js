d59cbdce03e988ecd3e0c8a0376c9421ea4e8287
Gene Conroy-Jones <drgenejones@gmail.com>
2017-02-21 19:11:32 -0800

    A new method for including all files in a directory.

--- webpack_plunge/client/webpack.js
+++ webpack_plunge/client/webpack.js
@@ -5,5 +5,18 @@ require("file-loader?name=landing.html!./app/landing.html");
 require("file-loader?name=home/about.html!./app/home/about.html");
 
 require('expose-loader?entry!./app/app.js');
-require('imports-loader?myApp=>entry.myApp!./app/landing.js');
-require('imports-loader?myApp=>entry.myApp!./app/home/about.js');
+
+// Old method include each file.
+// require('imports-loader?myApp=>entry.myApp!./app/landing.js');
+// require('imports-loader?myApp=>entry.myApp!./app/home/about.js');
+
+// New method include all files in a directory.
+function importAll (r) {
+    r.keys().forEach(function(file) {
+        if (!file.includes('app.js')) {
+            r(file);
+        }
+    });
+}
+importAll(require.context("imports-loader?myApp=>entry.myApp!./app/", true, /.*\.js/));
+
